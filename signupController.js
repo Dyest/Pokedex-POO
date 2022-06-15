2f8ae357b2c6656a136921ad6ded91b5996a0343
Dyest <marcoschneiders@live.com>
2022-06-14 22:15:03 -0300

    cad Controller

---
+++ signupController.js
@@ -0,0 +1,92 @@
+const form = document.getElementById("form");
+const username = document.getElementById("userName");
+const email = document.getElementById("userEmail");
+const password = document.getElementById("userPass");
+
+/*form.addEventListener("click", (e) => {
+
+  e.preventDefault();
+
+  checkInputs();
+
+});*/
+
+function checkName() {
+  const usernameValue = userName.value.trim();
+
+  if (usernameValue === "") {
+    // mostrar erro
+    // add classe
+    setErrorFor(username, "Preencha esse campo");
+    return false;
+  } else {
+    // adicionar a classe de sucesso
+    setSuccessFor(username);
+    return true;
+  }
+}
+
+function checkEmail() {
+  const emailValue = userEmail.value.trim();
+
+  if (emailValue === "") {
+    // mostrar erro
+    // add classe
+    setErrorFor(email, "Preencha esse campo");
+    return false;
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Email inválido");
+    return false;
+  } else {
+    // adicionar a classe de sucesso
+    setSuccessFor(email);
+    return true;
+  }
+}
+
+function checkPass() {
+  const passwordValue = userPass.value.trim();
+
+  if (passwordValue === "") {
+    // mostrar erro
+    // add classe
+    setErrorFor(password, "Preencha esse campo");
+    return false;
+  } else if (passwordValue.length < 6) {
+    setErrorFor(password, "Senha deve ter mais que 6 caracteres");
+    return false;
+  } else {
+    // adicionar a classe de sucesso
+    setSuccessFor(password);
+    return true;
+  }
+}
+
+function checkCad() {
+  if (checkName() == false || checkEmail() == false || checkPass() == false) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+
+  formControl.className = "textfield error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = "textfield success";
+}
+
+function isEmail(email) {
+  return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
+    email
+  );
+}
